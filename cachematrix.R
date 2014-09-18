378dc497bc3a5b92ccc8c94ec152cec49c7e771f
Marco Jardim <marquinho.jardim@gmail.com>
2014-09-18 17:40:31 -0300

    Prog Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+# makeCacheMatrix creates a list containing a function to
 
-## Write a short comment describing this function
 
+# required library by ginv() function, to invert M*N matrix
+library(MASS)
+
+# makeCacheMatrix creates a list with functions to get and set the inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(tempMatrix) {
+        x <<- tempMatrix
+        inv <<- NULL
+    }
+    get <- function() {
+		x
+	}
+    setInverse <- function(inverse) {
+		inv <<- inverse
+	}
+    getInverse <- function() { 
+		inv
+	}
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+# If the matrix returned on getInverse() has already been computed, return the cache. If not, compute it
+# return and store as cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInverse()
+    if(!is.null(inv)) {
+        return(inv)
+    }
+    data <- x$get()
+    inv <- ginv(data) # ginv() requires MASS library
+    x$setInverse(inv)
+    inv
 }
+
+
+# Sample run:
+# randomMatrix <- matrix( rnorm(10000*1000,mean=0,sd=1), 10000, 1000)
+# cache <- makeCacheMatrix(randomMatrix)
+# cacheSolve(cache)
+# cacheSolve(cache) ## should be faster because it's cached
